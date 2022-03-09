659597df15fb4e60dbcd7ead46958ab765e07aaa
Vishwajeet0708 <101243408+Vishwajeet0708@users.noreply.github.com>
2022-03-09 16:01:18 +0530

    Add files via upload

---
+++ TestMessage.java
@@ -0,0 +1,20 @@
+package Junit;
+
+import org.junit.Test;
+
+import org.junit.Assert;
+
+public class TestMessage {
+
+	
+		@Test 
+		public void testSayHello() {
+			System.out.println("Hiiiiiii");
+			Message m1 = new Message();
+			String act=m1.sayHello();
+			Assert.assertEquals("Hello", act);
+			}
+
+	}
+
+
